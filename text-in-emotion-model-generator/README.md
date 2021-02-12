6b240a486311fe5521623908ee7e592837f190d2
Oliver Meredith <mail@olivermeredith.com>
2021-02-12 14:14:49 +0000

    model generator readme + code comments

--- text-in-emotion-model-generator/README.md
+++ text-in-emotion-model-generator/README.md
@@ -1,2 +1,19 @@
-# TensorFlowJS-Chatbots
-TensorFlowJS Chatbot Examples
+# Train the tensorflow model
+
+This code generates a Tensorflow.js model that recognises emotional sentiment in text. 
+Created by following this [tutorial by Raphael Mun.](https://www.codeproject.com/Articles/5282690/AI-Chatbots-With-TensorFlow-js-Improved-Emotion-De)
+
+The model is trained using the GoEmotion dataset from Google Research.
+'GoEmotions is a corpus of 58k carefully curated comments extracted from Reddit, with human annotations to 27 emotion categories or Neutral.' - [Source](https://github.com/google-research/google-research/tree/master/goemotions)
+
+The model will begin training once the page has loaded, the model is then saved as a JSON file to your downloads folder. 
+
+The model can then be run immediately. 
+
+###Using the model
+This project already contains a trained model for you to use, but will still generate one automatically when loaded.
+After the model has been generated it will save to your downloads folder.
+
+To prevent the model being auto-generated and only have results generated on input, follow instructions outlined in the code as to which area to remove. 
+
+![alt text](./assets/training.png "Logo Title Text 1")
---
+++ text-in-emotion-model-generator/model.js
@@ -0,0 +1,152 @@
+const emotions = [
+    "admiration",
+    "amusement",
+    "anger",
+    "annoyance",
+    "approval",
+    "caring",
+    "confusion",
+    "curiosity",
+    "desire",
+    "disappointment",
+    "disapproval",
+    "disgust",
+    "embarrassment",
+    "excitement",
+    "fear",
+    "gratitude",
+    "grief",
+    "joy",
+    "love",
+    "nervousness",
+    "optimism",
+    "pride",
+    "realization",
+    "relief",
+    "remorse",
+    "sadness",
+    "surprise",
+    "neutral"
+];
+
+function setText(text) {
+    document.getElementById("status").innerText = text;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+(async () => {
+
+    //     // Load GoEmotions data (https://github.com/google-research/google-research/tree/master/goemotions)
+    let data = await fetch("web/emotions.tsv").then(r => r.text());
+    let lines = data.split("\n").filter(x => !!x); // Split & remove empty lines
+
+    // Randomize the lines
+    // --- CAN REMOVE AFTER TRAINING ---
+    shuffleArray(lines);
+
+    const numSamples = 200;
+    let sentences = lines.slice(0, numSamples).map(line => {
+        let sentence = line.split("\t")[0];
+        return sentence;
+    });
+
+
+    // --- CAN REMOVE AFTER TRAINING ---
+    let outputs = lines.slice(0, numSamples).map(line => {
+        let categories = line.split("\t")[1].split(",").map(x => parseInt(x));
+        let output = [];
+        for (let i = 0; i < emotions.length; i++) {
+            output.push(categories.includes(i) ? 1 : 0);
+        }
+        return output;
+    });
+    // --- END CAN REMOVE AFTER TRAINING ---
+
+    // Load the universal sentence encoder // DON'T REMOVE THIS
+    setText("Loading USE...");
+    let encoder = await use.load();
+    setText("Loaded!");
+    let embeddings = await encoder.embed(sentences);
+
+
+    // // --- MODEL TRAINING, REMOVE THIS IF MODEL HAS BEEN TRAINED AS JSON FILE ---
+    // // Define our model with several hidden layers
+    // const model = tf.sequential();
+    // model.add(tf.layers.dense({ units: 100, activation: "relu", inputShape: [512] }));
+    // model.add(tf.layers.dense({ units: 50, activation: "relu" }));
+    // model.add(tf.layers.dense({ units: 25, activation: "relu" }));
+    // model.add(tf.layers.dense({
+    //     units: emotions.length,
+    //     activation: "softmax"
+    // }));
+
+    // model.compile({
+    //     optimizer: tf.train.adam(),
+    //     loss: "categoricalCrossentropy",
+    //     metrics: ["accuracy"]
+    // });
+
+    // const xs = embeddings;
+    // const ys = tf.stack(outputs.map(x => tf.tensor1d(x)));
+    // await model.fit(xs, ys, {
+    //     epochs: 50,
+    //     shuffle: true,
+    //     callbacks: {
+    //         onEpochEnd: (epoch, logs) => {
+    //             setText(`Training... Epoch #${epoch} (${logs.acc})`);
+    //             console.log("Epoch #", epoch, logs);
+    //         }
+    //     }
+    // });
+    // const saveResult = await model.save('downloads://my-model');
+
+    // --- DO NOT REMOVE CODE BELOW THIS LINE ---
+
+    // CODE FROM HERE LOADS THE MODEL AND RUNS THE TENSORFLOW ALGORITHM ON EACH KEY PRESS, THE RESULT IS DISPLAY
+    const loadingStatus = document.querySelector('#loading_status');
+
+    async function analyseText() {
+        loadingStatus.innerText = "Loading....";
+
+        // UNCOMMENT THIS TO USE SAVED MODEL 
+
+        // const model = await tf.loadLayersModel('./my-model.json')
+
+        let text = document.getElementById('user-text').value;
+
+        document.getElementById("text").innerText = text;
+
+        let vector = await encoder.embed([text]);
+        let prediction = await model.predict(vector).data();
+        console.log({ prediction })
+
+        // Get the index of the highest value in the prediction
+        let id = prediction.indexOf(Math.max(...prediction));
+
+        let predictions = emotions.map((emotion, i) => {
+            return {
+                category: emotion,
+                value: prediction[i].toFixed(2)
+            }
+        })
+            .sort((a, b) => b.value - a.value);
+
+        document.querySelector('#results').innerText = JSON.stringify(predictions, null, 2)
+        console.log({ predictions })
+
+
+        setText(`Result: ${emotions[id]}`);
+
+        loadingStatus.innerText = "";
+    }
+
+    analyseText()
+    document.addEventListener('keyup', async () => analyseText());
+
+})();
