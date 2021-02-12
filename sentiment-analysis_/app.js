20111809b3837b2e7c20b5c0e37c6a7cecf8dcc7
Oliver Meredith <mail@olivermeredith.com>
2021-02-12 15:53:04 +0000

    Add sentiment-analysis express port

--- sentiment-analysis_/app.js
+++ sentiment-analysis_/app.js
@@ -8,6 +8,7 @@ var nlpRouter = require('./routes/nlp');
 
 var app = express();
 
+const PORT = process.env.PORT || 4000
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,4 +18,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/nlp', nlpRouter);
 
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
 module.exports = app;
