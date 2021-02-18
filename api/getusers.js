e7ecc0f05f4959bd06cf15d8cf48e3275ac91525
Oliver Meredith <mail@olivermeredith.com>
2021-02-18 17:58:35 +0000

    edit lambda

--- api/getusers.js
+++ api/getusers.js
@@ -1,9 +1,9 @@
 exports.handler = function (event, context, callback) {
 
-    const { name } = JSON.parse(event.body);
+    // const { name } = JSON.parse(event.body);
     event
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ message: `Hello ${name}` })
+        body: JSON.stringify({ message: `Hello there` })
     });
 }
---
+++ test/main.js
@@ -0,0 +1,16 @@
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({ "name": "Tom" });
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
+
+fetch("https://dazzling-mahavira-33ecc2.netlify.app/.netlify/functions/getusers", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
