90efbe176105333bbe1adda0eb72c0c26d6174a5
Oliver Meredith <mail@olivermeredith.com>
2021-02-18 17:50:06 +0000

    deploy first lambda

---
+++ api/getusers.js
@@ -0,0 +1,9 @@
+exports.handler = function (event, context, callback) {
+
+    const { name } = JSON.parse(event.body);
+    event
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Hello ${name}` })
+    });
+}
