caf64898bf1ac57a895fdff31fa8d68131ed7e64
Oliver Meredith <mail@olivermeredith.com>
2021-02-23 14:29:16 +0000

    re add push to mongo function

--- main/pages/api/push_tile.js
+++ main/pages/api/push_tile.js
@@ -20,13 +20,28 @@ import { connectToDatabase } from "../../util/mongodb";
 
 // };
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     console.log(req.method)
-    const { text, svg, data } = JSON.parse(req.body);
-    console.log("TEXT", text)
-    res.json({
-        text: text,
-        svg: svg,
-        data: data,
-    })
+    // const { text, svg, data } = JSON.parse(req.body);
+    // console.log("TEXT", text)
+    // res.json({
+    //     text: text,
+    //     svg: svg,
+    //     data: data,
+    // })
+    const { db } = await connectToDatabase();
+    let result = { test: true }
+
+    try {
+        await db
+            .collection("tiles_test")
+            .insertOne(JSON.parse(req.body), (err, result) => {
+                if (err) { res.json({ error: err }); }
+                res.json(result);
+            })
+
+    } catch (error) {
+        res.json({ error })
+    }
+
 }
